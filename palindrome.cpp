054522e2c4413dda2b03fa9b681764aff35a66bb
suhasakowshik <suhasakowshik@gmail.com>
2025-05-30 00:10:48 -0500

    Leed code practice problems day 1

---
+++ palindrome.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<string>
+
+bool checkIfPalindrom(std::string s)
+{
+    int left=0;
+    int right=s.size()-1;
+
+    while(left<right)
+    {
+        if(s[left]!=s[right])
+        {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+    return true;
+}
+
+int main()
+{
+    std::string s="suhas";
+
+    if(checkIfPalindrom(s))
+    {
+        std::cout<<s<<" is a palindrome"<<"\n";
+    }
+    else
+    {
+        std::cout<<s<<" is not a palindrome"<<"\n";
+    }
+    return 0;
+}
