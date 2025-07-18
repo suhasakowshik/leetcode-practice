cd049e44f7fca890a5a4a0c3944467f4930bf765
suhasakowshik <suhasakowshik@gmail.com>
2025-07-18 04:05:02 -0500

    day9

--- interview/ex1.cpp
+++ interview/ex1.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<vector>
 #include<algorithm>
+#include<memory>
 using namespace std;
 
 int main()
@@ -12,6 +13,22 @@ int main()
 
     cout<<*ptr1<<"\n";
     cout<<**ptr2<<"\n";
+
+    int* p=new int[10];
+    *p=5;
+
+    cout<<"P : "<<*p<<"\n";
+
+    delete p;
+
+    auto p1 = make_unique<int>(5);  // creates a unique_ptr managing an int initialized to 5
+
+    *p1 = 5;  // optional here, since it's already 5 from make_unique<int>(5)
+    
+    std::cout << "Value pointed by p1: " << *p1 << "\n";
+    std::cout << "Raw pointer (address): " << p1.get() << "\n";  // shows address, not value
+
+
 
     return 0;
 }
